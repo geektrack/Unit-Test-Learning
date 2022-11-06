951966e56f97d8f00c6c0d13a6d532cca14e3b41
geektrack <vasudev.rathore.github@outlook.com>
2022-11-06 21:02:52 +0530

    Initial commit

---
+++ Calculations.Tests/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Calculations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact] //Annotation used to represent a Test
+        public void Add_GivenTwoIntValues_ReturnsInt()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+            int expectedResult = 3;
+            //Act
+            var originalResult = calculator.Add(1, 2);
+
+            //Assert
+            Assert.Equal(expectedResult, originalResult);
+        }
+    }
+}
---
+++ Calculations/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+        }
+    }
+}
